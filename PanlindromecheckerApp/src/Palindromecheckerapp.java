35eb78afd7a2f12a24520776c61642cd3b721533
Havish242 <havish.munagala@gmail.com>
2026-02-26 14:19:17 +0530

    UC1: Application Entry & Welcome Message

---
+++ PanlindromecheckerApp/src/Palindromecheckerapp.java
@@ -0,0 +1,7 @@
+public class Palindromecheckerapp {
+    public static void main(){
+        System.out.println("Welcome to panlindrome checker app");
+        System.out.println("Version:1.0");
+        System.out.println("System initialized");
+    }
+}
